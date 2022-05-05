7be6efc5b39ad6944f79027cdb238a5f05a1b3ce
Swordzi <iiro.polso@protonmail.com>
2022-05-05 16:34:50 +0300

    stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.indigo,
+              child: Text('Chart'),
+              elevation: 5,
+            ),
+          ),
+          const Card(
+            color: Colors.red,
+            child: Text('List of TX'),
+          )
+        ],
+      ),
+    );
+  }
+}
